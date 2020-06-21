e9cd517cc51e9127eed0ceea513c539e3ecebbbf
Dominic Colson <dominic.colson@hotmail.co.uk>
2020-06-21 13:56:43 +0100

    again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Dominic from './Components/Cards/SingleCard'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Learn React</h2>
         <p>Dominic Colson</p>
+        <Dominic />
       </header>
     </div>
   );
---
+++ src/Components/Cards/SingleCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../Styles/SingleCard.css'
+
+class Dominic extends React.Component {
+    render(){
+        return (
+            <div>
+             <h2>Dominicddd</h2>
+            </div>
+        );
+    }
+}
+
+export default Dominic;
